71359b4c400376a433dfb622394a099455b0199f
jaka junaedi <jaka.junaedi03@gmail.com>
2021-08-31 22:52:52 +0700

    new edit UrlConfig

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,4 +1,4 @@
-const baseUrl = process.env.API || "https://nodejs-ecommerc.herokuapp.com/";
+const baseUrl = process.env.API || "https://nodejs-ecommerc.herokuapp.com";
 //const baseUrl = "http://localhost:2000";
 
 export const api = `${baseUrl}/api`;
